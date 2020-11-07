ce7c4e9efc7a164da11092565139077d67747639
Anirudh Pai <anirudhnpai@gmail.com>
2020-11-07 12:01:26 -0800

    Made more concise

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ image_path = 'data/test/non_fire/non_fire.195.png'
 
 def predict(image_path):
     cnn = load_model('model')
-    image = load_img(f'{image_path}', target_size=(128, 128))
+    image = load_img(image_path, target_size=(128, 128))
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     prediction = int(cnn.predict(image)[0][0])
